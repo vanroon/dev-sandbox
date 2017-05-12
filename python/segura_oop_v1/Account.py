0c4d3296c822a23a390718763531f2e558f87523
Erik van Roon <hfvanroon@outlook.com>
2017-05-12 06:43:37 +0000

    add class object counter

--- python/segura_oop_v1/Account.py
+++ python/segura_oop_v1/Account.py
@@ -1,5 +1,11 @@
 from abc import ABCMeta, abstractmethod
 
+#Class IterRegistry keeps track of all objects that are instantiated of a class
+class IterRegistry(type):
+	def __iter__(cls):
+		return iter(cls._registry)
+
+
 class Account(object):
 	"""Base class for accounts.
 	This class's metaclass is set to ABCMeta and one of its methods is virtuals. This
@@ -32,3 +38,39 @@ class CrossAccount(Account):
 	
 	def account_type(self):
 		return 'cross'
+class Transaction(object):
+	__metaclass__ = IterRegistry
+	_registry = []
+	
+	def __init__(self, SelfAccount, CrossAccount, amount):
+		"""Returns a new transaction"""
+		self.id = id 
+		self.SelfAccount = SelfAccount.iban
+		self.CrossAccount = CrossAccount.iban
+		self.currency = currency
+		self.processDate = processDate
+		self.debcred = debcred
+		self.amount = amount
+		self.description = description
+
+class Piggy(object):
+	__metaclass__ = IterRegistry
+	_registry = []
+	"""A Pigge (bank) consists of a set of transaction that fall under one category_code"""
+
+	def __init__(self, category_code, Transaction):
+		self.category_code = category_code
+		self.amount = Transaction.amount
+
+	# getBalance will returns the sum of a given set of transactions
+	def getBalance(category_code):
+		total = 0
+
+
+
+
+a = SelfAccount('NL44RABO1234567890')
+b = CrossAccount('SE32HAAS1932193210')
+print a.account_type()
+print b.account_type()
+
