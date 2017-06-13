1d6715674eaddb097829c8bf2238145cde86cb05
Erik from Pi <hfvanroon@gmail.com>
2017-06-13 18:30:29 +0000

    added aparen fix

--- python/segura/read_csv.py
+++ python/segura/read_csv.py
@@ -1,9 +1,11 @@
 import csv
 import MySQLdb
+import re
 
 # files and databases
 dump_file = '/home/pi/stack/SEGURA/Rabobank/MSSQL_Dump/total_dump_20170602.csv'
 database = 'SEGURA'
+p = re.compile("[1-9]{1}[0-9]{2}-[0-9]^")
 
 #Use this file to import a csv dump (pipe-separated) from MSSQL Segura DB
 
@@ -66,13 +68,36 @@ for row in csv_data:
 								'"%s", '\
 								'%s' \
 								');' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19])
+
+	if p.match(row[10]):
+		qry = 'INSERT INTO tblTransactionCategoryCodeMapping (' \
+					'masterId,' \
+					'categoryCode' \
+				') VALUES ( ' \
+					'%s, ' \
+					'"%s", '\
+					');' % (row[19], row[10][:5])
+
+	if row[10] in 'sparen':
+		sparenqry = 'INSERT INTO tblTransactionCategoryCodeMapping (' \
+						'masterId, ' \
+						'categorgCode' \
+					') VALUES ( ' \
+						'%s, ' \
+						'"%s", ' \
+					');' % (row[19], "103-0")
+
 	try:
 	
 		cursor.execute(sql)
+		cursor.execute(qry)
+		cursor.execute(sparenqry)
 		print "success"
 	except:
 		print "got error!"
 		print row[19]
-	print sql
+		print sql
+		print qry
+		print sparenqry
 mydb.commit()
 cursor.close()
